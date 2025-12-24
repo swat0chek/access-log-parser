e7de6c6a95008d6d6ccad3209733dd09d09028b0
Denis Korotov <korotov.deniz@ya.ru>
2025-12-24 11:23:36 +0300

    tast cikl 0.3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        String word = "infinitely";
+        int i = 0;
+        while (word != "Стоп") {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+
+            if (fileExists == true) {
+                boolean isDirectory = file.isDirectory();
+                if (isDirectory == false) {
+                    i++;
+                    System.out.println("Путь указан верно. Это файл номер " + i);
+                } else {
+                    System.out.println("Этот путь не до файла или неверно указан путь");
+                    continue;
+                }
+            } else {
+                System.out.println("Этот путь не до файла или неверно указан путь");
+                continue;
+            }
+        }
     }
 }
 
